8cd5938a15c9b7bdc66abe71eee5f59c196b44f1
berellevy <berellevy@gmail.com>
2020-09-15 10:58:21 -0400

    click me

--- App.js
+++ App.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { Text, View, Button } from 'react-native';
+import { 
+  View, 
+  ActivityIndicator, 
+  ProgressViewIOS, 
+  Button 
+} from 'react-native';
 
 export default function App() {
+  const [progress, setProgress] = useState(0.0)
+
+  const bumpProgress = () => {
+    
+    if (progress >= 1) {
+      setProgress(0)
+    } else {
+      setProgress(progress + 0.1)
+    }
+  }
+
+  const onButtonPress = () => {
+    console.log(`${new Date().toLocaleTimeString()} boop!`);
+    bumpProgress()
+  }
   return (
     <View style={{padding: 50}}>
-      <Text >Hello Yehuda</Text>
+      <ProgressViewIOS progress={progress} />
+      <ActivityIndicator size="large" color="#61DBFB" />
+      <Button title="click me" onPress={onButtonPress} />
     </View>
   )
 
